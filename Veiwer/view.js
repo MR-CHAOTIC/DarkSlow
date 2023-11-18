60945f5d6aea4f7b1f41159f544d3a5f5c3ba32d
MR.CHAOTIC <96962238+MR-CHAOTIC@users.noreply.github.com>
2023-11-17 20:57:01 -0600

    Update view.js

--- Veiwer/view.js
+++ Veiwer/view.js
@@ -1,1 +1,14 @@
-//
+/*
+    DarkSlow 2023-2024
+*/
+
+//Only works to view your own tabs right now, but where working on server capability (Pretty useless right now)
+
+//Get View
+setInterval(function () {
+    let user_view = location.href; //Get Current viewed tab
+    console.log(user_view); //For Tests
+    //Create Views (Show in DOM)
+    let view = "<iframe src='" + user_view + "' width='250' hieght='180'></iframe>";
+    document.getElementById("views").innerHTML = view;
+}, 10000);
