ba58775db2c58f0424ef9b2844c17554ee114b5a
MR.CHAOTIC <96962238+MR-CHAOTIC@users.noreply.github.com>
2023-11-21 22:08:51 -0600

    Update view.js

--- Veiwer/view.js
+++ Veiwer/view.js
@@ -4,25 +4,8 @@
 
 //Get View and update every 10 Seconds
 setInterval(function () {
+    
     let src = location.href;
-
     const user_view = "<iframe src='" + src + "' width='275' hieght='180'></iframe>";
-
-    let xhr = new XMLHttpRequest();
-    
-    //Create Response Header
-    xhr.open("POST", "Pre Hosted 'myView' link");
-    xhr.setRequestHeader("Content-Type","application/json");
-    
-    //Create JSON data
-    let re = {
-        view: user_view
-    }
-    
-    //Package JSON data
-    console.log(re);
-
-    //Send Data
-    xhr.send(re);
     
 }, 10000);
